1be5040f2ca2e2bba175b251c4c89420f8dab2da
hayden <haohaoalt@163.com>
2023-12-11 15:29:23 +0800

    feat: add thridParty

---
+++ install_dependence.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+# lesson3
+sudo apt-get install ros-melodic-csm
+
+# lesson4
+sudo apt-get install ros-melodic-laser-geometry
+
+# lesson6 install ros-melodic-g2o
+sudo apt-get install ros-melodic-libg2o
+
+# install ceres-solver-1.13.0
+cd thridParty/
+unzip ceres-solver-1.13.0.zip
+cd ceres-solver
+mkdir build
+cd build
+cmake .. 
+make
+
+# intsall gtsam-4.0.2
+cd ../../
+unzip gtsam-4.0.2.zip
+cd gtsam-4.0.2
+mkdir build
+cd build
+cmake .. 
+make
