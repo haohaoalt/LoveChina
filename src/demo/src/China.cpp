abab8ac281af0ee94923456d9faeb46fc5639cca
hayden <haohaoalt@163.com>
2024-01-13 20:53:07 +0800

    sorting code

--- src/demo/src/China.cpp
+++ src/demo/src/China.cpp
@@ -1,12 +1,3 @@
-/*
- * @Author: https://github.com/haohaoalt
- * @Date: 2023-08-26 21:50:07
- * @LastEditors: haohaoalt haohaoalt@163.com
- * @LastEditTime: 2023-08-26 21:54:24
- * @FilePath: /LoveChina/src/China.cpp
- * @Description: 
- * Copyright (c) 2023 by haohaoalt@163.com, All Rights Reserved. 
- */
 #include "China.h"
 
 void China::LoveChina() {
